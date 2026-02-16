e315d7fe8aeea3eaddbcaca0e8e93a12be23c35a
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-02-16 15:40:33 +0000

    Update eid_easter.py

--- eid_easter.py
+++ eid_easter.py
@@ -1,7 +1,11 @@
+# stdlib
 import datetime
+
+# 3rd party
 import matplotlib.pyplot as plt
 from domdf_python_tools.dates import calc_easter
-from hijri_converter import Hijri
+from hijri_converter import Gregorian, Hijri
+
 
 def calc_easter_eastern(year: int) -> datetime.date:
 	"""
@@ -13,10 +17,11 @@ def calc_easter_eastern(year: int) -> datetime.date:
 	a = year % 4
 	b = year % 7
 	c = year % 19
-	d = ((19*c) + 15) % 30
-	e = ((2*a) + (4*b) - d + 34) % 7
+	d = ((19 * c) + 15) % 30
+	e = ((2 * a) + (4 * b) - d + 34) % 7
 	month = (d + e + 114) // 31
-	day =  ((d + e + 114) % 31) + 1
+	day = ((d + e + 114) % 31) + 1
+
 	return datetime.date(year, month, day) + datetime.timedelta(days=13)
 
 deltas = {}  # Islamic year to days since easter that year
@@ -31,19 +36,20 @@ for islamic_year in range(1343, 1501):
 	easter_date_gregorian = calc_easter(gregorian_year)
 	print("Easter:", easter_date_gregorian)
 
-	delta = (eid_date_gregorian-easter_date_gregorian).days
+	delta = (eid_date_gregorian - easter_date_gregorian).days
 	print("Delta:", delta, "days after Easter")
 	print()
 
 	deltas[islamic_year] = delta
 
-
 fig = plt.figure(figsize=(8, 4))
 plt.plot(list(deltas.keys()), list(deltas.values()))
 plt.title("Number of days later Eid al-Fitr occurs in the Islamic year than Easter")
-plt.xlabel("Islamic Year (2025 AD = 1446 AH)")
+plt.xlabel(f"Islamic Year ({current_gregorian_year} AD = {current_islamic_year} AH)")
 plt.ylabel("Days difference (-ve = before Easter)")
 plt.xlim(1340, 1500)
 plt.tight_layout()
-#plt.savefig("eid_easter.png")
+plt.savefig("eid_easter.png")
+plt.minorticks_on()
+plt.grid(True, "both")
 plt.show()
